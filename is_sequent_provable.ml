5c5a694d20d8c7dac4a9ddaa0ed41779def13aea
Olivier Laurent <olivier.laurent@ens-lyon.fr>
2021-04-25 18:01:06 +0200

    provability check through balanced additive slices

--- is_sequent_provable.ml
+++ is_sequent_provable.ml
@@ -1,7 +1,8 @@
 let is_sequent_provable_with_exceptions request_as_json =
     let sequent = Raw_sequent.sequent_from_json request_as_json in
-    if not (Clauses.provable_sequent_as_classical sequent) then false
-    else Phase.valid_sequent sequent;;
+    (Clauses.provable_sequent_as_classical sequent) &&
+    (Phase.valid_sequent sequent) &&
+    (Slice.provable_sequent_as_slices sequent);;
 
 let is_sequent_provable request_as_json =
     try let is_provable = is_sequent_provable_with_exceptions request_as_json in
---
+++ slice.ml
@@ -0,0 +1,43 @@
+open Sequent
+
+(* Balanced additive slices: provability check *)
+
+let prod_map f l1 l2 =
+  List.concat_map (fun x1 -> List.map (f x1) l2) l1
+
+let rec slices = function
+  | One -> [[[]]]
+  | Bottom -> [[[]]]
+  | Top -> []
+  | Zero -> [[]]
+  | Litt x -> [[[(x, true)]]]
+  | Dual x -> [[[(x, false)]]]
+  | Tensor (f1, f2) -> prod_map (prod_map List.append) (slices f1) (slices f2)
+  | Par (f1, f2) -> prod_map (prod_map List.append) (slices f1) (slices f2)
+  | With (f1, f2) -> slices f1 @ (slices f2)
+  | Plus (f1, f2) -> prod_map List.append (slices f1) (slices f2)
+  | Ofcourse f -> slices f
+  | Whynot f -> []
+
+let rec remove x = function
+  | [] -> raise Not_found
+  | h :: tl -> if x = h then tl
+               else h :: (remove x tl)
+
+let rec valid_slice = function
+  | [] -> true
+  | (x, b) :: xs -> try valid_slice (remove (x, not b) xs)
+                    with Not_found -> false
+
+let litteral_compare (x1, b1) (x2, b2) =
+  let cx = compare x1 x2 in
+  if cx = 0 then compare b1 b2 else cx
+
+let sorted_slices f =
+  List.map (List.map (List.sort litteral_compare)) (slices f)
+
+let valid_slices f =
+  List.for_all (List.exists valid_slice) (sorted_slices f)
+
+let provable_sequent_as_slices sequent =
+  valid_slices (sequent_to_formula sequent)
