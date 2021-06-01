570eb7c02fd30c4a0aae4479797bbbd8c362a7f1
Etienne Callies <atnk@hotmail.fr>
2021-06-01 09:52:19 +0200

    let lollipop and dual for notation display

--- parse_sequent.ml
+++ parse_sequent.ml
@@ -1,9 +1,9 @@
 let ll_parse sequent_as_string =
-    let raw_sequent = Ll_parser.main Ll_lexer.token (Lexing.from_string sequent_as_string) in
-    Raw_sequent.to_sequent raw_sequent;;
+    Ll_parser.main Ll_lexer.token (Lexing.from_string sequent_as_string);;
 
 let safe_parse sequent_as_string =
-    try let sequent = ll_parse sequent_as_string in
+    try let raw_sequent = ll_parse sequent_as_string in
+        let sequent = Raw_sequent.to_sequent raw_sequent in
         let proof = Proof.Hypothesis_proof sequent in
         true, Proof.to_json proof
     with t -> if Printexc.to_string t = "Stdlib.Parsing.Parse_error"
@@ -12,9 +12,9 @@ let safe_parse sequent_as_string =
         else false, `String ("Technical error: " ^ Printexc.to_string t);;
 
 let safe_parse_formula formula_as_string =
-    try let sequent = ll_parse formula_as_string in
-        match sequent with
-        | [e] -> true, Raw_sequent.formula_to_json e
+    try let raw_sequent = ll_parse formula_as_string in
+        match raw_sequent with
+        | {hyp=[]; cons=[e]} -> true, Raw_sequent.raw_formula_to_json e
         | _ -> false, `String "Input must contain exactly one formula."
     with t -> if Printexc.to_string t = "Stdlib.Parsing.Parse_error"
             || Printexc.to_string t = "Failure(\"lexing: empty token\")" then
@@ -22,8 +22,8 @@ let safe_parse_formula formula_as_string =
         else false, `String ("Technical error: " ^ Printexc.to_string t);;
 
 let safe_is_valid_litt litt =
-    try let sequent = ll_parse litt in
-        match sequent with
-        | [Litt s] -> true
+    try let raw_sequent = ll_parse litt in
+        match raw_sequent with
+        | {hyp=[]; cons=[Litt s]} -> true
         | _ -> false
     with _ -> false;;
