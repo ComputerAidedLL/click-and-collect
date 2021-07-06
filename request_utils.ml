49e0f4ee6b3b20f811812d3b2e1010fe9ad44c36
Etienne Callies <atnk@hotmail.fr>
2021-07-06 18:40:54 +0200

    Apply reversible first

--- request_utils.ml
+++ request_utils.ml
@@ -13,8 +13,3 @@ let get_string d k =
     let value = get_key d k in
     try Yojson.Basic.Util.to_string value
     with Yojson.Basic.Util.Type_error (_, _) -> raise (Bad_request_exception ("field '" ^ k ^ "' must be a string"))
-
-let get_raw_formula d k =
-    let value = get_key d k in
-    try Raw_sequent.json_to_raw_formula value
-    with Raw_sequent.Json_exception m -> raise (Bad_request_exception ("field '" ^ k ^ "' must contain a valid formula: " ^ m))
